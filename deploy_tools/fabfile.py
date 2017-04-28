e28a7c0e1964d669882c7f167c2cc0ed4b064d1e
Mika <mika.tuomala@hotmail.com>
2017-04-28 17:10:13 +0300

    Päivitetty fab filen github osoite

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -2,7 +2,7 @@ from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run
 import random
 
-REPO_URL = 'https://github.com/hjwp/book-example.git'  
+REPO_URL = 'https://github.com/MikaTml/superlist.git'  
 
 def deploy():
     site_folder = f'/home/{env.user}/sites/{env.host}'   
