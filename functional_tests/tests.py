99cd5b504088310ba436c32d9850147ca8d84dcb
Mika <mika.tuomala@hotmail.com>
2017-05-20 14:21:35 +0300

    Testit päivitetty

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 import time, os
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
+from unittest import skip
 
 MAX_WAIT = 5 
 
@@ -35,6 +36,23 @@ class NewVisitorTest(StaticLiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
         
+    def test_cannot_add_empty_list_items(self):
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
+
+        # The home page refreshes, and there is an error message saying
+        # that list items cannot be blank
+
+        # She tries again with some text for the item, which now works
+
+        # Perversely, she now decides to submit a second blank list item
+
+        # She receives a similar warning on the list page
+
+        # And she can correct it by filling some text in
+        @skip
+        self.fail('write me!')
+    
     
     #def test_can_start_a_list_and_retrieve_it_later(self):
     def test_can_start_a_list_for_one_user(self):       
